e2a8136db4d176fde377a92ffd3f8e6605fe6241
Thiago Jose Avelar Silva <tavelars@outlook.com>
2018-01-06 17:10:22 -0200

    Associacao Produto Categoria

--- src/main/java/br/com/threeout/CursomcApplication.java
+++ src/main/java/br/com/threeout/CursomcApplication.java
@@ -8,13 +8,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import br.com.threeout.domain.Categoria;
+import br.com.threeout.domain.Produto;
 import br.com.threeout.repositories.CategoriaRepository;
+import br.com.threeout.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -25,8 +30,21 @@ public class CursomcApplication implements CommandLineRunner {
 		
 		Categoria categoriaInformatica = new Categoria(null, "Informática");
 		Categoria categoriaEscritorio = new Categoria(null, "Escritório");
-		categoriaRepository.save(Arrays.asList(categoriaInformatica, categoriaEscritorio));
 		
+		Produto produto1 = new Produto(null, "Computador", 2000.0);
+		Produto produto2 = new Produto(null, "Impressora", 800.0);
+		Produto produto3 = new Produto(null, "Mouse", 30.0);
+		
+		categoriaInformatica.getProdutos().addAll(Arrays.asList(produto1, produto2, produto3));
+		categoriaInformatica.getProdutos().addAll(Arrays.asList(produto2));
+		
+		produto1.getCategorias().add(categoriaInformatica);
+		produto2.getCategorias().addAll(Arrays.asList(categoriaInformatica, categoriaEscritorio));
+		produto3.getCategorias().add(categoriaInformatica);
+		
+		
+		categoriaRepository.save(Arrays.asList(categoriaInformatica, categoriaEscritorio));
+		produtoRepository.save(Arrays.asList(produto1, produto2, produto3));
 		
 	}
 }
--- src/main/java/br/com/threeout/domain/Produto.java
+++ src/main/java/br/com/threeout/domain/Produto.java
@@ -8,29 +8,33 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
 @Entity
-public class Categoria implements Serializable {
+public class Produto implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-
-	public Categoria() {
-	}
-
-	public Categoria(Integer id, String nome) {
-		this.id = id;
-		this.nome = nome;
-	}
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
-
 	private String nome;
+	private Double preco;
+	
+	@ManyToMany
+	@JoinTable(name="PRODUTO_CATEGORIA", joinColumns = @JoinColumn(name="PRODUTO_ID"), inverseJoinColumns = @JoinColumn(name="CATEGORIA_ID"))
+	private List<Categoria> categorias = new ArrayList<Categoria>();
 
-	@ManyToMany(mappedBy="categorias")
-	private List<Produto> produtos = new ArrayList<Produto>();
+	public Produto() {
+	}
+
+	public Produto(Integer id, String nome, Double preco) {
+		this.id = id;
+		this.nome = nome;
+		this.preco = preco;
+	}
 
 	public Integer getId() {
 		return id;
@@ -40,12 +44,28 @@ public class Categoria implements Serializable {
 		this.id = id;
 	}
 
-	public List<Produto> getProdutos() {
-		return produtos;
+	public String getNome() {
+		return nome;
 	}
 
-	public void setProdutos(List<Produto> produtos) {
-		this.produtos = produtos;
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
+	public List<Categoria> getCategorias() {
+		return categorias;
+	}
+
+	public void setCategorias(List<Categoria> categorias) {
+		this.categorias = categorias;
 	}
 
 	@Override
@@ -64,21 +84,13 @@ public class Categoria implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Categoria other = (Categoria) obj;
+		Produto other = (Produto) obj;
 		if (id == null) {
 			if (other.id != null)
 				return false;
 		} else if (!id.equals(other.id))
 			return false;
 		return true;
-	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
 	}
 
 }
