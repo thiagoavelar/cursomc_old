5ab20ee2b600ea179edc4af846d39c953bc8e445
Thiago Jose Avelar Silva <tavelars@outlook.com>
2018-01-06 10:55:39 -0200

    Teste classe de Dominio

--- src/main/java/br/com/threeout/resources/CategoriaResources.java
+++ src/main/java/br/com/threeout/resources/CategoriaResources.java
@@ -1,16 +1,31 @@
 package br.com.threeout.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.threeout.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResources {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "Rest Categoria está funcionando";
+	public List<Categoria> listar() {
+		
+		Categoria categoria1 = new Categoria(1, "Informática");
+		Categoria categoria2 = new Categoria(1, "Escritório");
+		
+		List<Categoria> categorias = new ArrayList<Categoria>();
+		categorias.add(categoria1);
+		categorias.add(categoria2);
+		
+		return categorias;
+		
+		
 	}
 	
 }
