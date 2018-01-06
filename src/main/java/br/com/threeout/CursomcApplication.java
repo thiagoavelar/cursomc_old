31b946c76c7a547e314b9fe94523dba38bc3b924
Thiago Jose Avelar Silva <tavelars@outlook.com>
2018-01-06 12:27:03 -0200

    Povoador na classe application

--- src/main/java/br/com/threeout/CursomcApplication.java
+++ src/main/java/br/com/threeout/CursomcApplication.java
@@ -1,12 +1,32 @@
 package br.com.threeout;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import br.com.threeout.domain.Categoria;
+import br.com.threeout.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... arg0) throws Exception {
+		
+		Categoria categoriaInformatica = new Categoria(null, "Informática");
+		Categoria categoriaEscritorio = new Categoria(null, "Escritório");
+		categoriaRepository.save(Arrays.asList(categoriaInformatica, categoriaEscritorio));
+		
+		
 	}
 }
