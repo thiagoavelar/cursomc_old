3441947ef9f5f6696476ae666400f445a5b8bb55
Thiago Jose Avelar Silva <tavelars@outlook.com>
2018-01-06 10:39:08 -0200

    Configuracao Rest Categoria

---
+++ src/main/java/br/com/threeout/resources/CategoriaResources.java
@@ -0,0 +1,16 @@
+package br.com.threeout.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/categorias")
+public class CategoriaResources {
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public String listar() {
+		return "Rest Categoria está funcionando";
+	}
+	
+}
