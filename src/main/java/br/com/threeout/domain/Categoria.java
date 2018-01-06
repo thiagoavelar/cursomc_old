ae3ca7041ee32e3b8ee848c8028ce043b1eb889e
Thiago Jose Avelar Silva <tavelars@outlook.com>
2018-01-06 11:31:53 -0200

    Configuracao do banco de dados em memória

--- src/main/java/br/com/threeout/domain/Categoria.java
+++ src/main/java/br/com/threeout/domain/Categoria.java
@@ -2,6 +2,12 @@ package br.com.threeout.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Categoria implements Serializable {
 
 	private static final long serialVersionUID = 1L;
@@ -13,8 +19,11 @@ public class Categoria implements Serializable {
 		this.id = id;
 		this.nome = nome;
 	}
-
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
+	
 	private String nome;
 	
 	public Integer getId() {
