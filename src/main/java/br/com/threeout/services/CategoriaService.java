306d59e88964d7aec1b1eec9e9ecbd6935626eb2
Thiago Jose Avelar Silva <tavelars@outlook.com>
2018-01-07 18:10:49 -0200

    Cadastro Exception

--- src/main/java/br/com/threeout/services/CategoriaService.java
+++ src/main/java/br/com/threeout/services/CategoriaService.java
@@ -5,16 +5,21 @@ import org.springframework.stereotype.Service;
 
 import br.com.threeout.domain.Categoria;
 import br.com.threeout.repositories.CategoriaRepository;
+import br.com.threeout.services.exception.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
-	
+
 	@Autowired
 	private CategoriaRepository categoriaRepository;
-	
+
 	public Categoria buscar(Integer id) {
-		Categoria categoria =  categoriaRepository.findOne(id);
+		Categoria categoria = categoriaRepository.findOne(id);
+		if (categoria == null) {
+			throw new ObjectNotFoundException(
+					"Objeto não encotrado! Id: " + id + " Tipo: " + Categoria.class.getName());
+		}
 		return categoria;
 	}
-	
+
 }
